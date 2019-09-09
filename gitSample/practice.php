2cae8e9c3721517f26191cbb2aa267df927d9cbc
mayumi <dffxw608@gmail.com>
2019-09-09 14:32:16 +0000

    first commit

---
+++ gitSample/practice.php
@@ -0,0 +1,47 @@
+<?php
+echo 'hello php!';
+?>
+
+<?php
+ $a=3; $b=7;
+ echo $a+$b;
+ ?>
+ 
+<?php 
+ $array_month=[
+     "1月","2月","3月", "4月","5月","6月","7月","8月","9月","10月", "11月","12月"
+ ];
+ echo $array_month[7];
+ ?>
+ 
+ <?php
+  $hello = "Hello,";
+  $name = "Mayumi";
+  $world = "'s World!";
+  echo $hello . $name . $world;
+  ?>
+  
+  <?php
+  $tech_boost = "tech";cd
+   $tech_boost .= ' boost';
+   echo $tech_boost;
+   ?>
+   
+   <?php
+   $calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+   ];
+   echo $calendar_2018 ["December"];
+   ?>
+   
