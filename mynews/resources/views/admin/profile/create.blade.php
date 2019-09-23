66b01fb3ebeaa486eb0ce26022e1c8f110108af9
mayumi <dffxw608@gmail.com>
2019-09-23 11:46:46 +0000

    kadai11

--- mynews/resources/views/admin/profile/create.blade.php
+++ mynews/resources/views/admin/profile/create.blade.php
@@ -1,14 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>profile</title>
-    </head>
-    <body>
-        <h1>Profile作成画面</h1>
-    </body>
-    
-</html>
+
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'profile'を埋め込む --}}
+@section('title', ' My profile ')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>profile作成画面</h2>
+            </div>
+        </div>
+    </div>
+@endsection
